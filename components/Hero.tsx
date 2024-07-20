a03d21a22a6535d07844394f582cdc92abcb94ec
Boris <boris.vicena@gmail.com>
2024-07-20 23:30:21 +0200

    Hero section almost done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { ModeToggle } from "./ui/ThemeToggle";
+import { GridBackground } from "./ui/GridBackground";
+import { TextGenerateEffect } from "./ui/TextGenerate";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
-        <h1>Hello, GoSocial</h1>
+        <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
+      </div>
+      <div>
+        <GridBackground />
+      </div>
+      <div className="relative flex justify-center my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[50vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-sm text-center text-blue-100 max-w-80">
+            MULTI-DIGITAL SOLUTION AGENCY
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-5xl"
+            words="Outstanding Digital Solutions for Marketing and Web Development"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl text-gray-500">
+            Let us elevate your online presence and drive your success.
+          </p>
+          <a href="#about">
+            <MagicButton title="Learn more" icon={<FaLocationArrow />} position="right" />
+          </a>
+        </div>
       </div>
     </div>
   );
