bea212df68d449db3eea13047dd831fc6a984d34
Boris <boris.vicena@gmail.com>
2024-07-22 12:34:21 +0200

    refactor(text): Removed useless text

--- components/About.tsx
+++ components/About.tsx
@@ -3,11 +3,14 @@ import React from "react";
 const About = () => {
   return (
     <section id="about">
-      <div className="relative py-20 h-[100vh]">
-        <h2 className="dark:text-white text-black-100 heading">We strive for better digital future</h2>
-        <div className="flex flex-wrap items-center justify-center">
-          <p>hello</p>
-        </div>
+      <div className="relative py-20 h-[100vh] max-w-7xl w-full">
+        <h2 className="dark:text-white text-black-100 heading">
+          We strive for better{" "}
+          <span className="bg-gradient-to-br from-[#9e65f4] dark:to-[#cbacf9] to-purple-700 bg-clip-text text-transparent">
+            {" "}
+            digital future
+          </span>
+        </h2>
       </div>
     </section>
   );
--- components/KeyStatistics.tsx
+++ components/KeyStatistics.tsx
@@ -4,30 +4,22 @@ import { gridItems } from "@/data";
 
 const KeyStatistics = () => {
   return (
-    <div className="py-20 lg:py-40 max-w-7xl w-full">
-      <div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-5 mx-auto">
-          {gridItems.map(({ id, title, value, description }) => (
-            <div
-              key={id}
-              className="relative bg-gradient-to-b from-[#9e65f4] to-purple-700 p-6 rounded-lg overflow-hidden"
-            >
-              <Grid size={20} />
-              <p className="text-4xl font-bold text-white">{value}</p>
-              <p className="text-base font-light uppercase tracking-wider dark:text-neutral-200 text-neutral-100 relative z-20">
-                {title}
-              </p>
-              <p className="text-neutral-200 mt-4 text-base font-normal relative z-20">{description}</p>
-            </div>
-          ))}
-        </div>
-      </div>{" "}
-      <p className="text-black-100 dark:text-white font-bold text-3xl mt-10">
-        We like to talk{" "}
-        <span className="bg-gradient-to-br from-[#9e65f4] dark:to-[#cbacf9] to-purple-700 bg-clip-text text-transparent">
-          numbers
-        </span>
-      </p>
+    <div className="py-20 lg:py-30 max-w-7xl w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-5 mx-auto">
+        {gridItems.map(({ id, title, value, description }) => (
+          <div
+            key={id}
+            className="relative bg-gradient-to-b from-[#9e65f4] to-purple-700 p-6 rounded-lg overflow-hidden"
+          >
+            <Grid size={20} />
+            <p className="text-4xl font-bold text-white">{value}</p>
+            <p className="text-base font-light uppercase tracking-wider dark:text-neutral-200 text-neutral-100 relative z-20">
+              {title}
+            </p>
+            <p className="text-neutral-200 mt-4 text-base font-normal relative z-20">{description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
