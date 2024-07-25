7381fb14fac0bf5f31b3ea0c69bd517fa31b5478
Boris <boris.vicena@gmail.com>
2024-07-25 07:22:37 +0200

    refactor(data): Moved data from about.tsx to index.ts

--- components/About.tsx
+++ components/About.tsx
@@ -3,71 +3,7 @@ import { Lamp } from "./ui/Lamp";
 import { HoverEffect } from "./ui/CardHover";
 import { FlipWords } from "./ui/FlipWords";
 import { FaCaretRight } from "react-icons/fa";
-
-export const services = [
-  {
-    title: "Web Development",
-    description: [
-      "Custom Website Design and Development",
-      "E-commerce Solutions",
-      "Content Management System (CMS)",
-      "Website Maintenance and Support",
-    ],
-    link: "/web-development",
-  },
-  {
-    title: "Mobile App Development",
-    description: [
-      "iOS and Android App Development",
-      "Cross-platform App Development",
-      "Mobile App UI/UX Design",
-      "App Store Optimization (ASO)",
-    ],
-    link: "mobile-app-development",
-  },
-  {
-    title: "Digital Marketing",
-    description: [
-      "Search Engine Optimization (SEO)",
-      "Pay-Per-Click (PPC) Advertising",
-      "Social Media Marketing (SMM)",
-      "Email Marketing Campaigns",
-    ],
-    link: "digital-marketing",
-  },
-  {
-    title: "Content Creation",
-    description: [
-      "Copywriting and Blogging",
-      "Video Production and Editing",
-      "Graphic Design and Infographics",
-      "Photography Services",
-    ],
-    link: "content-creation",
-  },
-  {
-    title: "Branding & Identity",
-    description: [
-      "Logo Design and Brand Identity",
-      "Brand Strategy and Consulting",
-      "Brand Guidelines Development",
-      "Print and Digital Design",
-    ],
-    link: "branding-and-identity",
-  },
-  {
-    title: "UI/UX Design",
-    description: [
-      "User Interface (UI) Design",
-      "User Experience (UX) Research and Testing",
-      "Prototyping and Wireframing",
-      "Usability Testing",
-    ],
-    link: "ui-and-ux-design",
-  },
-];
-
-const words = ["website", "design", "marketing", "brand", "content", "strategy"];
+import { flipWords, services } from "@/data";
 
 const About = () => {
   return (
@@ -79,7 +15,7 @@ const About = () => {
         <div className="h-[10rem] flex justify-start items-center px-4">
           <div className="text-5xl font-medium bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text text-transparent leading-tight">
             Build your
-            <FlipWords words={words} /> <br />
+            <FlipWords words={flipWords} /> <br />
             with GoSocial
           </div>
         </div>
--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,4 @@
+// Items for grid
 export const gridItems = [
   {
     id: 0,
@@ -25,5 +26,71 @@ export const gridItems = [
     value: "200%",
     description:
       "Users experience up to a 200% increase in engagement rates with our advanced analytics and optimized posting strategies.",
+  },
+];
+// Words for flipping animation
+export const flipWords = ["website", "strategy", "design", "brand", "content"];
+
+// Services
+export const services = [
+  {
+    title: "Web Development",
+    description: [
+      "Custom Website Design and Development",
+      "E-commerce Solutions",
+      "Content Management System (CMS)",
+      "Website Maintenance and Support",
+    ],
+    link: "/web-development",
+  },
+  {
+    title: "Mobile App Development",
+    description: [
+      "iOS and Android App Development",
+      "Cross-platform App Development",
+      "Mobile App UI/UX Design",
+      "App Store Optimization (ASO)",
+    ],
+    link: "mobile-app-development",
+  },
+  {
+    title: "Digital Marketing",
+    description: [
+      "Search Engine Optimization (SEO)",
+      "Pay-Per-Click (PPC) Advertising",
+      "Social Media Marketing (SMM)",
+      "Email Marketing Campaigns",
+    ],
+    link: "digital-marketing",
+  },
+  {
+    title: "Content Creation",
+    description: [
+      "Copywriting and Blogging",
+      "Video Production and Editing",
+      "Graphic Design and Infographics",
+      "Photography Services",
+    ],
+    link: "content-creation",
+  },
+  {
+    title: "Branding & Identity",
+    description: [
+      "Logo Design and Brand Identity",
+      "Brand Strategy and Consulting",
+      "Brand Guidelines Development",
+      "Print and Digital Design",
+    ],
+    link: "branding-and-identity",
+  },
+  {
+    title: "UI/UX Design",
+    description: [
+      "User Interface (UI) Design",
+      "User Experience (UX) Research and Testing",
+      "Prototyping and Wireframing",
+      "Usability Testing",
+    ],
+    link: "ui-and-ux-design",
   },
 ];
