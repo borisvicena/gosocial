2ad89b3367f6500340234de70aaa4fafcea655f0
Boris <boris.vicena@gmail.com>
2024-07-26 19:01:32 +0200

    feat(stats): Improved key stats card color and text

--- components/KeyStatistics.tsx
+++ components/KeyStatistics.tsx
@@ -7,16 +7,17 @@ const KeyStatistics = () => {
     <div className="pt-20 lg:pt-30 max-w-7xl mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-5 mx-auto">
         {gridItems.map(({ id, title, value, description }) => (
-          <div
-            key={id}
-            className="relative bg-gradient-to-br from-[#DA22FF] to-[#9733EE] p-6 rounded-lg overflow-hidden"
-          >
+          <div key={id} className="relative bg-[#191c2f] p-6 rounded-lg overflow-hidden">
             <Grid size={20} />
-            <p className="text-4xl font-bold text-white">{value}</p>
-            <p className="text-base font-light uppercase tracking-wider dark:text-neutral-200 text-neutral-100 relative z-20">
+            <p className="text-4xl font-bold text-transparent bg-gradient-to-r from-[#4776E6] to-[#8E54E9] bg-clip-text">
+              {value}
+            </p>
+            <p className="text-base font-light uppercase tracking-wider dark:text-neutral-200 text-white relative z-20">
               {title}
             </p>
-            <p className="text-slate-200 mt-4 text-base font-normal relative z-20">{description}</p>
+            <p className="text-slate-100 mt-4 text-base font-extralight tracking-wide leading-snug relative z-20">
+              {description}
+            </p>
           </div>
         ))}
       </div>
