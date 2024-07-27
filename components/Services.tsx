688fb98d46e1175cdf04aaa7747a35cdaa3cbe72
Boris <boris.vicena@gmail.com>
2024-07-27 17:51:56 +0200

    Updated spotlight opacity

--- components/Services.tsx
+++ components/Services.tsx
@@ -6,11 +6,13 @@ import { HeartHandshakeIcon, Icon } from "lucide-react";
 import { FiTarget } from "react-icons/fi";
 import { FaCheck } from "react-icons/fa6";
 import { IoMdCheckmark, IoMdCheckmarkCircle, IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { BsArrowRight } from "react-icons/bs";
+import { Spotlight } from "./ui/Spotlight";
 
 const Services = () => {
   return (
     <section id="services" className="border-b border-white/[0.2]">
-      <div className="pb-20 lg:pb-30 max-w-7xl mx-auto">
+      <div className="relative pb-20 lg:pb-30 max-w-7xl mx-auto">
         <div className="h-[10rem] flex justify-start items-center">
           <div className="text-5xl font-bold bg-gradient-to-br from-slate-50 to-slate-500 bg-clip-text text-transparent leading-tight">
             Our mission is clear:
@@ -20,9 +22,12 @@ const Services = () => {
           </div>
         </div>
         <div className="relative pb-10 max-w-xl">
-          <p className="text-slate-400 text-base leading-relaxed">
+          <p className="relative text-slate-400 text-base leading-relaxed inline">
             We create digital solutions from A to Z adaptable to any kind of business. Our team is dedicated to
-            enhancing your online presence and achieving your goals.
+            enhancing your online presence and achieving your goals.{" "}
+            <a href="#" className="inline-flex items-center gap-2 text-white text-sm font-semibold">
+              Contact Us <BsArrowRight />
+            </a>
           </p>
         </div>
         <div className="max-w-7xl mx-auto">
