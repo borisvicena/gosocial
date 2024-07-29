8c7949e3b92f04762c4665525efc2845d487925a
Boris <boris.vicena@gmail.com>
2024-07-29 17:47:41 +0200

    feat(footer): Added LightFooter component from AuraUI lib

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import LightFooter from "./ui/LightFooter";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center py-20 lg:pb-32 border-t border-t-border">
-      <p className="dark:text-white font-light uppercase">Footer section</p>
-    </div>
+    <>
+      <LightFooter />
+    </>
   );
 };
 
