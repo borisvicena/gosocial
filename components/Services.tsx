82ec3b2bce581d5b72c4095b7ad945a49428c4f5
Boris <boris.vicena@gmail.com>
2024-07-30 13:56:54 +0200

    fix(services): Fixed section layout and improved responsivness

--- components/Services.tsx
+++ components/Services.tsx
@@ -13,16 +13,14 @@ const Services = () => {
   return (
     <section id="services" className="border-b border-white/[0.2]">
       <div className="relative py-20 lg:py-32 max-w-7xl mx-auto">
-        <div className="h-[10rem] flex justify-start items-center">
-          <div className="text-5xl font-bold bg-gradient-to-br from-slate-50 to-slate-500 bg-clip-text text-transparent leading-tight">
+        <div className="flex flex-col justify-start items-start px-4 lg:px-0">
+          <div className="text-5xl justify-start font-bold bg-gradient-to-br from-slate-50 to-slate-500 bg-clip-text text-transparent leading-tight">
             Our mission is clear:
             <br />
             build your{" "}
             <span className="bg-gradient-to-br from-[#FF512F] to-[#F09819] bg-clip-text">online success</span>
           </div>
-        </div>
-        <div className="relative pb-10 max-w-xl">
-          <p className="relative text-slate-400 text-base leading-relaxed inline">
+          <p className="text-slate-400 text-base leading-relaxed mt-4 mb-8 max-w-xl">
             We create digital solutions from A to Z adaptable to any kind of business. Our team is dedicated to
             enhancing your online presence and achieving your goals.{" "}
             <a href="#" className="inline-flex items-center gap-2 text-white text-sm font-semibold">
