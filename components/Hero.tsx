e42e0a4300ad2ec059acc19873914b7cae864087
Boris <boris.vicena@gmail.com>
2024-07-30 14:23:28 +0200

    Improved Hero sectiono

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,33 +7,33 @@ import { FaArrowRightLong, FaRegCompass } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className="relative pb-20 pt-36 max-w-7xl mx-auto">
-      <div>
-        <Spotlight className="-top-10 left-0" fill="white" />
-      </div>
-      <div>
+    <section id="hero" className="">
+      <div className="relative py-20 lg:py-32">
+        <Spotlight className="-top-10 left-40" fill="white" />
         <GridBackground />
-      </div>
-      <div className="relative flex justify-center my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[50vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-sm text-center dark:text-blue-100 text-blue-900 max-w-80">
-            MULTI-DIGITAL SOLUTION AGENCY
-          </h2>
-          <HeadingUI />
-          <p className="text-center md:tracking-wider my-4 text-sm md:text-lg lg:text-xl text-slate-400">
-            Let us elevate your online presence and drive your success.
-          </p>
-          <div className="inline-flex gap-4 my-10">
-            <a href="#services">
-              <ButtonUI text="Learn more" variant="secondary" size="large" icon={<FaRegCompass />} />
-            </a>
-            <a href="/contact">
-              <ButtonUI text="Contact" variant="none" size="large" icon={<FaArrowRightLong />} />
-            </a>
+        <div className="relative max-w-7xl mx-auto">
+          <div className="relative flex justify-center mt-20 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[50vw] flex flex-col items-center justify-center">
+              <h2 className="uppercase tracking-widest text-sm text-center dark:text-blue-100 text-blue-900 max-w-80">
+                MULTI-DIGITAL SOLUTION AGENCY
+              </h2>
+              <HeadingUI />
+              <p className="text-center md:tracking-wider my-4 text-sm md:text-lg lg:text-xl text-slate-400">
+                Let us elevate your online presence and drive your success.
+              </p>
+              <div className="inline-flex gap-4 mt-10">
+                <a href="#services">
+                  <ButtonUI text="Learn more" variant="secondary" size="large" icon={<FaRegCompass />} />
+                </a>
+                <a href="/contact">
+                  <ButtonUI text="Contact" variant="none" size="large" icon={<FaArrowRightLong />} />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- components/KeyStatistics.tsx
+++ components/KeyStatistics.tsx
@@ -4,23 +4,25 @@ import { gridItems } from "@/data";
 
 const KeyStatistics = () => {
   return (
-    <div className="pt-20 lg:pt-30 max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-5 mx-auto">
-        {gridItems.map(({ id, title, value, description }) => (
-          <div
-            key={id}
-            className="relative bg-gradient-to-br from-[#191c2f] to-slate-800 p-6 rounded-lg overflow-hidden"
-          >
-            <Grid size={20} />
-            <p className="text-4xl font-bold text-transparent bg-gradient-to-r from-[#4776E6] to-[#8E54E9] bg-clip-text">
-              {value}
-            </p>
-            <p className="text-base font-light uppercase tracking-wider dark:text-white relative z-20">{title}</p>
-            <p className="dark:text-slate-400 mt-4 text-sm tracking-wide leading-snug relative z-20">{description}</p>
-          </div>
-        ))}
+    <section id="stats">
+      <div className="relative pt-20 lg:pt-32 max-w-7xl mx-auto px-2 lg:px-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mx-auto">
+          {gridItems.map(({ id, title, value, description }) => (
+            <div
+              key={id}
+              className="relative bg-gradient-to-br from-[#191c2f] to-slate-800 p-4 rounded-lg overflow-hidden border dark:border-white/[0.2]"
+            >
+              <Grid size={20} />
+              <p className="text-4xl font-bold text-transparent bg-gradient-to-r from-[#4776E6] to-[#8E54E9] bg-clip-text">
+                {value}
+              </p>
+              <p className="text-base font-light uppercase tracking-wider dark:text-white relative z-20">{title}</p>
+              <p className="dark:text-slate-400 mt-4 text-sm tracking-wide leading-snug relative z-20">{description}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
