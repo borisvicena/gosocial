727e215ea5bb0cbed75e6f732ba59118efd39aa7
Boris <boris.vicena@gmail.com>
2024-08-10 12:36:43 +0200

    feat(animation): Added animation to key statistics cards

--- components/KeyStatistics.tsx
+++ components/KeyStatistics.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import { Grid } from "./ui/FeaturesSection";
 import { gridItems } from "@/data";
+import { motion } from "framer-motion";
 
 const KeyStatistics = () => {
   return (
@@ -8,8 +10,16 @@ const KeyStatistics = () => {
       <div className="relative pt-20 lg:pt-32 max-w-7xl mx-auto px-2 lg:px-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mx-auto">
           {gridItems.map(({ id, title, value, description }) => (
-            <div
+            <motion.div
               key={id}
+              initial={{ opacity: 0, scale: 0.5 }}
+              whileInView={{ opacity: 1 }}
+              animate={{ scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
               className="relative bg-gradient-to-br from-[#191c2f] to-slate-800 p-4 rounded-lg overflow-hidden border dark:border-white/[0.2]"
             >
               <Grid size={20} />
@@ -18,7 +28,7 @@ const KeyStatistics = () => {
               </p>
               <p className="text-base font-light uppercase tracking-wider dark:text-white relative z-20">{title}</p>
               <p className="dark:text-slate-400 mt-4 text-sm tracking-wide leading-snug relative z-20">{description}</p>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
